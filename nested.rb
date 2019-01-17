4253684348ffd8569426d397b22e6865a11c38f7
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-17 17:45:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,9 +87,9 @@ def adding_matz
         }
      }
      
-   #  programmer_hash[:yukihiro_matsumoto] = {}
-    # programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-    # programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"] 
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"] 
        
     programmer_hash[:yukihiro_matsumoto] = {
       "known_for": "Ruby",
